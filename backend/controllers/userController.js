55a75cc6124f49389bb9ee989f83e94bdcc86460
Harsh180303 <harshchourasiya6261@gmail.com>
2025-06-23 22:18:45 +0530

    feat: buit profile route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,26 +1,69 @@
-import User from "../models/userModel.js"
+import User from '../models/userModel.js'
+import uploadOnCloudinary from '../config/cloudinary.js'
 
 export const getCurrentUser = async (req, res, next) => {
-    try {
-        const userId = req.userId
-        const user = await User.findById({_id: userId})
+  try {
+    const userId = req.userId
+    const user = await User.findById({ _id: userId })
 
-        if(!user) {
-            return res.status(404).json({
-                success: false,
-                message: "User not found"
-            })
-        }
-
-        return res.status(200).json({
-            success: true,
-            message: "Current user found",
-            user,
-        })
-    } catch (error) {
-        return res.status(500).json({
-            success: false,
-            message: `${error.message} || "Unable to find current user" `
-        })
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: 'User not found',
+      })
     }
+
+    return res.status(200).json({
+      success: true,
+      message: 'Current user found',
+      user,
+    })
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: `${error.message} || "Unable to find current user" `,
+    })
+  }
+}
+
+
+export const editProfile = async (req, res, next) => {
+  try {
+    const { name, about } = req.body
+    let image
+    if (req.file) {
+      image = await uploadOnCloudinary(req.file.path)
+
+      if (!image) {
+        return res
+          .status(500)
+          .json({ success: false, message: 'Image upload failed, Try again.' })
+      }
+    }
+
+    const updateData = {}
+    if (name) updateData.name = name
+    if (about) updateData.about = about
+    if (image) updateData.image = image
+
+    const user = await User.findByIdAndUpdate(
+      req.user._id,
+      updateData,
+      { new: true }
+    )
+
+    if(!user) {
+        return res.status(404).json({ success: false, message: 'User not found' })
+    }
+
+    return res
+      .status(200)
+      .json({ success: false, message: 'Profile updated successfully', user })
+  } catch (error) {
+    next({
+        statusCode: 500,
+        message: "Failed to update profile",
+        error: error.message
+    })
+  }
 }
