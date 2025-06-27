741ebf916bc94bb4eb44d1b48f1c2a6532d5ab34
Harsh180303 <harshchourasiya6261@gmail.com>
2025-06-27 23:38:35 +0530

    temp: integrate dummy data for testing ChatWindow user details

---
+++ frontend/static/dummyChats.js
@@ -0,0 +1,37 @@
+const dummyChats = [
+    {
+    _id: "1",
+    userName: "john_doe",
+    name: "John Doe",
+    image: "",
+    lastMessage: "Hey, what's up?",
+    time: "10:45 AM",
+    message_count: 5,
+    pin: false,
+    mute: true
+  },
+  {
+    _id: "2",
+    userName: "jane_smith",
+    name: "Jane Smith",
+    image: "",
+    lastMessage: "Let’s catch up later! and this is for testing perpouse only to check the data not flowing out",
+    time: "9:30 AM",
+    message_count: 19,
+    pin: true,
+    mute: true
+  },
+  {
+    _id: "3",
+    userName: "alex99",
+    name: "Alex",
+    image: "",
+    lastMessage: "Good night!",
+    time: "Yesterday",
+    message_count: 2,
+    pin: false,
+    mute: false
+  }
+]
+
+export default dummyChats
