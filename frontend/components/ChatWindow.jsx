9bf7b503d65f948147e3dc99c4d89347aa5e427b
Harsh180303 <harshchourasiya6261@gmail.com>
2025-07-07 22:31:48 +0530

    feat: build send message functionality

--- frontend/components/ChatWindow.jsx
+++ frontend/components/ChatWindow.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react'
+import axios from 'axios'
 import EmojiPicker from 'emoji-picker-react';
 import { useDispatch, useSelector } from 'react-redux'
 import dp from '../src/assets/dp.png'
@@ -57,12 +58,44 @@ function ChatWindow() {
   const emojiIconRef = useRef(null)
   const attachmentsRef = useRef(null)
   const attachmentsIconRef = useRef(null)
+  const imageInputRef = useRef(null)
+  const videoInputRef = useRef(null)
+  const audioInputRef = useRef(null)
+  const documentInputRef = useRef(null)
   const { selectedChat } = useSelector((state) => state.chat)
   const dispatch = useDispatch()
   const [isAttachmentOpen, setIsAttachmentOpen] = useState(false)
   console.log(selectedChat)
   const [showEmojiPicker, setShowEmojiPicker] = useState(false)
   const [message, setMessage] = useState('')
+  const [mediaFile, setMediaFile] = useState(null)
+
+  const handleMediaSelect = (e) => {
+    // console.log(e)
+    const file = e.target.files[0]
+    console.log(file)
+    if(!file) return;
+
+    setMediaFile(file) //ready to send
+    // e.target.value = '' // clear input
+  }
+
+  const handleAttachmentClick = (type) => {
+    if(type === 'image') {
+      imageInputRef.current.click(); // opens file picker
+    } else if(type === 'video') {
+      videoInputRef.current.click();
+    } else if(type === 'audio') {
+      audioInputRef.current.click();
+    } else{
+      const docType = ['.pdf', '.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx', '.txt']
+      if (docType.includes(type)) {
+      documentInputRef.current.click()
+    }
+    }
+
+    // to be add other types here
+  }
 
   useEffect(() => {
     const handleClickOutside = (event) => {
@@ -83,6 +116,40 @@ function ChatWindow() {
       document.removeEventListener('mousedown', handleClickOutside)
     }
   }, [])
+
+  const getMediaType = (file) => {
+    const type = file.type;
+    if (type.startsWith('image/')) return 'image'    
+    if (type.startsWith('video/')) return 'video'    
+    if (type.startsWith('audio/')) return 'audio'  
+    return 'file'  
+  }
+
+  const handleSendMessage = async () => {
+    if (!message.trim() && !mediaFile) return
+
+    const formData = new FormData
+    formData.append('content', message.trim())
+    formData.append('messageType', mediaFile ? getMediaType(mediaFile) : 'text')
+    if (mediaFile) {
+      formData.append('file', mediaFile)  // backend mai media name se h to check
+    }
+
+    try {
+      const response = await axios.post(`${import.meta.env.VITE_API_BASE_URL}/message/send/${selectedChat._id}`, formData, {
+        // headers: {
+        //   'Content-Type': 'multipart/form-data',
+        // },
+        withCredentials: true,
+      })
+
+      console.log('Message send: ', response)
+      setMessage('');
+      setMediaFile(null)
+    } catch (error) {
+      console.error('Error sending message: ', error)
+    }
+  }
 
   return (
     <div className="hidden lg:flex w-full flex-1 h-full bg-[#2c2125]">
@@ -123,12 +190,12 @@ function ChatWindow() {
           </div>
 
           {/* MESSAGE AREA */}
-          <div className="h-full flex-1 overflow-y-auto pb-[10rem] px-4 pt-2">
-            <SenderMessage />
+          <div className="h-full flex-1 overflow-y-auto space-y-3 pb-[10rem] px-4 pt-2">
+            {/* <SenderMessage />
             <ReceiverMessage />
             <SenderMessage />
             <ReceiverMessage />
-            <SenderMessage />
+            <SenderMessage /> */}
           </div>
 
           {/* Bottom Most form */}
@@ -146,6 +213,7 @@ function ChatWindow() {
                 >
                   <FaPlus className="text-white" />
                 </div>
+
                 {isAttachmentOpen && (
                   <div
                     ref={attachmentsRef}
@@ -153,6 +221,7 @@ function ChatWindow() {
                     <div className="grid grid-cols-3 sm:grid-cols-3 gap-x-2 gap-y-2">
                       {attachments.map((item, index) => (
                         <div
+                          onClick={() => handleAttachmentClick(item.type)}
                           key={index}
                           className="flex flex-col items-center justify-center bg-[#161717] p-3 rounded-md min-w-[4.5rem] h-[5rem] cursor-pointer hover:bg-[#1e1e1e] transition-all"
                         >
@@ -191,6 +260,43 @@ function ChatWindow() {
                 )}
               </div>
 
+              {/* IMAGE INPUT */}
+              <input 
+                ref={imageInputRef}
+                type='file'
+                accept='image/*'
+                hidden
+                onChange={handleMediaSelect}
+              />
+
+              {/* VIDEO INPUT */}
+              <input 
+                ref={videoInputRef}
+                type='file'
+                accept='video/*'
+                hidden
+                onChange={handleMediaSelect}
+              />
+
+              {/* AUDIO INPUT */}
+              <input 
+                ref={audioInputRef}
+                type='file'
+                accept='audio/*'
+                hidden
+                onChange={handleMediaSelect}
+              />
+
+              {/* DOCUMENT INPUT */}
+              <input 
+                ref={documentInputRef}
+                type='file'
+                // accept='*/*'
+                accept=".pdf,.doc,.docx,.xls,.xlsx,.ppt,.pptx,.txt"
+                hidden
+                onChange={handleMediaSelect}
+              />
+
               {/* INPUT BOX */}
               <div className="flex items-center gap-x-3 w-full">
                 <textarea
@@ -206,7 +312,9 @@ function ChatWindow() {
                 {message === '' ? (
                   <MdMic className="h-6 w-6 cursor-pointer hover:text-[#CA4F00]" />
                 ) : (
-                  <IoMdSend className="h-6 w-6 cursor-pointer hover:text-[#CA4F00]" />
+                  <IoMdSend 
+                    onClick={handleSendMessage}
+                    className="h-6 w-6 cursor-pointer hover:text-[#CA4F00]" />
                 )}
               </div>
             </form>
--- frontend/components/ReceiverMessage.jsx
+++ frontend/components/ReceiverMessage.jsx
@@ -5,7 +5,6 @@ function ReceiverMessage() {
   return (
       <div className='w-fit max-w-[25rem] xl:max-w-[25rem] 2xl:max-w-[30rem] h-fit flex flex-col text-start bg-[#242626] text-white rounded-tl-none rounded-2xl px-5 py-2 relative left-0 mr-auto shadow-black shadow-md'>
           {/* <h1>Sender Message is to check the word limit of this box is  acturally the fgive exqula o tor not</h1> */}
-          <img src={dp} width={150} className='rounded-lg'/>
           <h1>Sender Message is to check the word limit of this box is  acturally the fgive exqula o tor not</h1>
   
       </div>
