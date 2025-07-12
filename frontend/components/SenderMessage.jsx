a3d48f601ebb7c314b9a6ffa0ac41c8a9dcbff9e
Harsh180303 <harshchourasiya6261@gmail.com>
2025-07-12 23:36:04 +0530

    fix(ui): fixed SenderMessage.jsx and ReceiverMessage.jsx

--- frontend/components/SenderMessage.jsx
+++ frontend/components/SenderMessage.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
-import dp from '../src/assets/dp.png'
 
 function SenderMessage({ msg }) {
   return (
     <div className='w-fit max-w-[25rem] xl:max-w-[25rem] 2xl:max-w-[30rem] h-fit flex flex-col text-start bg-[#CA4F00]/70 text-white  rounded-tr-none rounded-2xl px-5 py-2 relative right-0 ml-auto shadow-black shadow-md'>
-        {console.log('MESSAGE: ', msg)}
-        {/* {msg.msg.messageType === 'image' ? <img src='' /> : } */}
-        {/* <p>{msg.msg.content}</p> */}
+        {msg.messageType === 'image' && msg.media?.url && (
+          <img src={msg?.media?.url} alt='sent' className='w-full max-w-[16rem] rounded-md my-2'/>
+        )}
+
+        {msg.content && (
+          <p>{msg.content}</p>
+        )}
+        
     </div>
   )
 }
