f960fb2eab27f26eba703628f0679e2de73c8a48
Harsh180303 <harshchourasiya6261@gmail.com>
2025-07-13 23:21:23 +0530

    style: improved preview ui

--- frontend/components/ChatWindow/MediaPreview.jsx
+++ frontend/components/ChatWindow/MediaPreview.jsx
@@ -8,19 +8,20 @@ function MediaPreview({mediaFile, onClear}) {
   const fileURL = URL.createObjectURL(mediaFile)
   
   return (
-    <div className='absolute bottom-19 left-0 bg-[#161717] w-[17rem]'>
+    <div className='absolute bottom-19 left-0 bg-[#161717] p-6 pt-2 rounded-md flex flex-col'>
       {console.log("MEdiaFile : ",mediaFile)}
 
       <button
         onClick={onClear}
+        className='flex justify-end cursor-pointer'
       >
-        <RxCross2 />
+        <RxCross2 className=' hover:bg-red-600 w-fit'/>
       </button>
 
       {
         type.startsWith('image/') && (
           <img src={fileURL} alt='Preview'
-            className='max-w-[13rem] flex justify-center rounded-md'
+            className='max-w-[13rem] flex justify-center rounded-md mt-1'
           />
         )
       }
