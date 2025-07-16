e8d65f847d176fe2af8ff5c14b8b15ba7b2be696
Harsh180303 <harshchourasiya6261@gmail.com>
2025-07-16 22:22:53 +0530

    feat: installed socket.io for client and improved socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,7 +1,22 @@
 import http from 'http'
 import express from 'express'
+import { Server } from 'socket.io'
 const app = express()
 
 const server = http.createServer(app)
 
-export { app, server }
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:5173'
+    }
+})
+
+io.on('connection', (socket) => {
+    console.log(`User connected: ${socket.id}`)
+
+    socket.on('disconnect', (reason) => {
+        console.log(`socket ${socket.id} disconnected due to ${reason}`)
+    })
+})
+
+export { app, server, io }
